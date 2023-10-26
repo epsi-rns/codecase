f75e0262098a5db14ad8c543564d60ba7e8bd604
epsi sayidina <epsi.nurwijayadi@gmail.com>
2023-10-26 16:47:26 +0700

    Article: Pivot - openPyXL - Pivot Writer: fix index

--- python/openpyxl/pivot-cats/lib/PivotWriter.py
+++ python/openpyxl/pivot-cats/lib/PivotWriter.py
@@ -129,7 +129,7 @@ class PivotWriter(BaseWriter):
     col_pos = self.col1 + len_col + 1
 
     cell = self.sheet.cell(row_pos, col_pos)
-    cell.value     = int(row['Total'])
+    cell.value     = int(row['Category'])
 
     cell.alignment = self.centerText
     cell.border    = self.left_border
@@ -172,7 +172,7 @@ class PivotWriter(BaseWriter):
     row_pos = self.row1 + len_row + 1
 
     cell = self.sheet.cell(row_pos, col_pos)
-    cell.value = int(self.total_row['Total'])
+    cell.value = int(self.total_row['Category'])
 
     cell.font      = self.headerFont
     cell.alignment = self.centerText
@@ -199,3 +199,4 @@ class PivotWriter(BaseWriter):
     self.write_column_headers()
     self.write_rows()
     self.write_column_total()
+
